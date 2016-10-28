0f9f58ccedae0541f3f3d9aad1c79f03d5fd5881
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2016-10-28 00:19:02 -0200

    Updated resource

--- Shared/Networking/Resource.swift
+++ Shared/Networking/Resource.swift
@@ -1,11 +1,13 @@
-struct Resource<A> {
+public typealias JSONDictionary = [String: AnyObject]
+
+public struct Resource<A> {
     let url: NSURL
     let parse: (Data) -> A?
 }
 
-extension Resource {
+public extension Resource {
 
-    init(url: NSURL, parseJSON: @escaping (Any) -> A?) {
+    public init(url: NSURL, parseJSON: @escaping (Any) -> A?) {
         self.url = url
         self.parse = { data in
             let json = try? JSONSerialization.jsonObject(with: data as Data, options: [])
