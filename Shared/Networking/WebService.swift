272703b72b70c86bbac2af0f24478dc26b4bb743
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2016-10-29 10:27:26 -0200

    Updated web service to render error without resource

--- Shared/Networking/WebService.swift
+++ Shared/Networking/WebService.swift
@@ -6,7 +6,10 @@ final class WebServiceSession: WebService {
 
     func load<T>(resource: Resource<T>, completion: @escaping (T?, Error?) -> ()) {
         URLSession.shared.dataTask(with: resource.url as URL) { data, _, error in
-            guard let data = data else { return }
+            guard let data = data else {
+                completion(nil, error)
+                return
+            }
             completion(resource.parse(data), error)
         }.resume()
     }
